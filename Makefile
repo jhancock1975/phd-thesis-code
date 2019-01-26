262709eabc01ef750a4319efa2de1142ec6a7b49
John Hancock <jhancock1975@gmail.com>
2019-01-26 15:55:03 -0500

    use null for initial value of node data

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ linked-list: linked-list.o
 	$(CC) -o linked-list linked-list.o
 
 linked-list.o: linked-list.cpp
-	$(CC) -c -o linked-list.o linked-list.cpp
+	$(CC) -c -o linked-list.o  linked-list.cpp
 
 clean: linked-list linked-list.o
 	rm linked-list
--- linked-list.cpp
+++ linked-list.cpp
@@ -16,37 +16,42 @@ public:
 
   void setData(int node_val){
     this->node_val = node_val;
-    printf("node_val=%d\n", this->node_val);
-
   }
   
     
 private:
   Node *next;
-  int node_val;
+  int node_val = NULL;
 };
 
 class LinkedList{
 public:
   LinkedList(){
     head = (Node *) malloc(sizeof(Node));
-    head->setNext( NULL);
+    head->setNext(NULL);
   }
   
   void add(int node_val){
     Node* cur = head;
-    while(cur->getNext() != NULL){
+    while (cur->getNext() != NULL){
       cur = cur->getNext();
     }
-    Node *new_node = (Node *)malloc(sizeof(Node));
-    new_node->setData(node_val);
-    new_node->setNext(NULL);
-    cur->setNext(new_node);
+    if (cur->getData() == NULL){
+      cur->setData(node_val);
+    } else { 
+      Node *new_node = (Node *)malloc(sizeof(Node));
+      new_node->setData(node_val);
+      new_node->setNext(NULL);
+      cur->setNext(new_node);
+    }
   }
 
+  /**
+   * prints list contents to console
+   */
   void printList(){
     Node* cur = head;
-    while(cur->getNext() != NULL){
+    while(cur != NULL){
       printf("%d, ", cur->getData());
       cur = cur->getNext();
     }
